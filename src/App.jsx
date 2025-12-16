b801fd849539013e594aa5fa1ea32ef87f5e2f6c
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-12-17 00:13:19 +0530

    Reorganize imports and restore Login route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Products from "./pages/Products";
 import Cart from "./pages/Cart";
-import Login from "./pages/Login";
 import Orders from "./pages/Orders";
-import Categories from "./pages/Categories";
-import CategoryProducts from "./pages/CategoryProducts";
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
@@ -13,11 +11,9 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Products />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="/category/:id" element={<CategoryProducts />} />
         <Route path="/orders" element={<Orders />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
