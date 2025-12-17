e719a18c2d859fc4265cf6ccf9cbe30c0d7aae1f
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-12-17 15:13:39 +0530

    Update SubCategories.jsx

--- src/pages/SubCategories.jsx
+++ src/pages/SubCategories.jsx
@@ -1,22 +1,32 @@
-import { useParams, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
 import api from "../api";
 
 export default function SubCategories() {
   const { categoryId } = useParams();
-  const [subs, setSubs] = useState([]);
+  const [subCategories, setSubCategories] = useState([]);
 
   useEffect(() => {
-    api.get(`/subcategories?category=${categoryId}`)
-      .then(res => setSubs(res.data));
+    api
+      .get(`/subcategories?category=${categoryId}`)
+      .then(res => setSubCategories(res.data))
+      .catch(err => {
+        console.error(err);
+        alert("Failed to load subcategories");
+      });
   }, [categoryId]);
 
   return (
     <div style={{ padding: 20 }}>
       <h2>Sub Categories</h2>
-      {subs.map(s => (
-        <div key={s._id}>
-          <Link to={`/products/${s._id}`}>{s.name}</Link>
+
+      {subCategories.length === 0 && <p>No subcategories found</p>}
+
+      {subCategories.map(sub => (
+        <div key={sub._id} style={{ marginBottom: 10 }}>
+          <Link to={`/products/${sub._id}`}>
+            {sub.name}
+          </Link>
         </div>
       ))}
     </div>
