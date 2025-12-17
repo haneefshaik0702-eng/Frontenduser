0d6c7969d51033f72548a03fa400a1bb1fba8931
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-12-17 17:48:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
-
-// pages
 import Folders from "./pages/Folders";
 import Vendors from "./pages/Vendors";
 import Categories from "./pages/Categories";
-import SubCategories from "./pages/SubCategories";
+import Subcategories from "./pages/Subcategories";
 import Products from "./pages/Products";
-
 import Cart from "./pages/Cart";
 import Orders from "./pages/Orders";
 import Login from "./pages/Login";
@@ -17,18 +14,12 @@ export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
-
       <Routes>
-        {/* ENTRY */}
         <Route path="/" element={<Folders />} />
-
-        {/* FLOW */}
-        <Route path="/folders/:folderId" element={<Vendors />} />
-        <Route path="/vendors/:vendorId" element={<Categories />} />
-        <Route path="/categories/:categoryId" element={<SubCategories />} />
-        <Route path="/products/:subId" element={<Products />} />
-
-        {/* USER */}
+        <Route path="/folder/:folderId/vendors" element={<Vendors />} />
+        <Route path="/vendor/:vendorId/categories" element={<Categories />} />
+        <Route path="/category/:categoryId/subcategories" element={<Subcategories />} />
+        <Route path="/subcategory/:subcategoryId/products" element={<Products />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/orders" element={<Orders />} />
         <Route path="/login" element={<Login />} />
