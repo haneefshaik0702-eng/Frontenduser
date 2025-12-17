9ade3ad351e7b2ede3b884b01cdefa376c69417e
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-12-17 15:05:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
-import Products from "./pages/Products";
+
+// pages
+import Folders from "./pages/Folders";
+import Vendors from "./pages/Vendors";
 import Categories from "./pages/Categories";
-import CategoryProducts from "./pages/CategoryProducts";
+import SubCategories from "./pages/SubCategories";
+import Products from "./pages/Products";
+
 import Cart from "./pages/Cart";
 import Orders from "./pages/Orders";
 import Login from "./pages/Login";
@@ -12,10 +17,18 @@ export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
+
       <Routes>
-        <Route path="/" element={<Products />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="/category/:id" element={<CategoryProducts />} />
+        {/* ENTRY */}
+        <Route path="/" element={<Folders />} />
+
+        {/* FLOW */}
+        <Route path="/folders/:folderId" element={<Vendors />} />
+        <Route path="/vendors/:vendorId" element={<Categories />} />
+        <Route path="/categories/:categoryId" element={<SubCategories />} />
+        <Route path="/products/:subId" element={<Products />} />
+
+        {/* USER */}
         <Route path="/cart" element={<Cart />} />
         <Route path="/orders" element={<Orders />} />
         <Route path="/login" element={<Login />} />
