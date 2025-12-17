a19b8caa21c6f7a1f5bdb02b29a98d5cff3e4cb7
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-12-17 14:28:08 +0530

    Update Categories.jsx

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,17 +1,19 @@
 import { useEffect, useState } from "react";
-import api from "../api";
 import { Link } from "react-router-dom";
+import api from "../api";
 
 export default function Categories() {
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    api.get("/categories").then(res => setCategories(res.data));
+    api.get("/categories")
+      .then(res => setCategories(res.data));
   }, []);
 
   return (
-    <div>
+    <div style={{ padding: 20 }}>
       <h2>Categories</h2>
+
       {categories.map(c => (
         <div key={c._id}>
           <Link to={`/category/${c._id}`}>{c.name}</Link>
