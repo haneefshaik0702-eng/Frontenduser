cc0cc56679ba0778e3fcac12d3cd80ba415c17bf
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-12-19 10:36:27 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,16 @@
 import axios from "axios";
 
-export default axios.create({
-  baseURL: "https://backend-ynph.onrender.com/api"
+const API = axios.create({
+  baseURL: "https://backend-ynph.onrender.com/api",
 });
+
+// Add token automatically to protected requests
+export const setAuthToken = (token) => {
+  if (token) {
+    API.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else {
+    delete API.defaults.headers.common["Authorization"];
+  }
+};
+
+export default API;
